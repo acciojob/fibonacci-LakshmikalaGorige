fb90fdd3876e1c3a26574209d21fbe48226196a4
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-30 20:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+// your code here 
+	let fib = [];
+	fib[1] = 0;
+	fib[2] = 1;
+	
+	if(num > 2){
+
+		for(let i=2; i<num; i++)
+		{
+			fib[i+1] = fib[i] + fib[i-1];
+		}
+		
+	}
+
+	return fib[num];
+	
 }
+console.log(fibonacci(4));
 
 module.exports = fibonacci;
